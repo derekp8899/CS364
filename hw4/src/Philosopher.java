267453f045eb70593b2f640b6c63abccc7173aed
Derek P <>
2019-03-26 17:19:22 -0500

    filled out constructors and instance variables

--- hw4/src/Philosopher.java
+++ hw4/src/Philosopher.java
@@ -11,18 +11,43 @@ uses JMS messaging to determine when forks are available
 
 */
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
+
 public class Philosopher implements Runnable{
 
-    public void Philosopher(){
+    private boolean stop;
+    private int id;
+    private MessageConsumer consumer;
+    private MessageProducer waiter;
+    private double timeTot;
+    private double timeCrit;
+    private int meals;
+    private int thoughts;
 
-
-
+    public void Philosopher(MessageConsumer inQueue, MessageProducer wait, int id){
+	this.stop = false;
+	this.id = id;
+	this.consumer = inQueue;
+	this.waiter = wait;
+	timeTot = 0;
+	timeCrit = 0;
+	meals = 0;
+	thoughts = 0;
     }
 	
     public void run(){
 
-
+	while(!stop){
+	    
+	}
 
     }
 
+    public void setStop(boolean flag){
+	stop = flag;
+    }
 }
--- hw4/src/Waiter.java
+++ hw4/src/Waiter.java
@@ -10,18 +10,107 @@ contains fork usage data and signals philosophers when they are available to pic
 
 */
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
+
 public class Waiter implements Runnable{
+    
+    private boolean stop;
+    private boolean[] forks;
+    private MessageConsumer consumer;
+    private MessageProducer aProd;
+    private MessageProducer bProd;
+    private MessageProducer cProd;
+    private MessageProducer dProd;
+    private MessageProducer eProd;
 
-    public void Waiter(){
-
-
+    public void Waiter(MessageProducer alpha, MessageProducer bravo, MessageProducer charlie, MessageProducer delta, 
+		       MessageProducer echo, MessageConsumer inQueue){
+	aProd = alpha;
+	bProd = bravo;
+	cProd = charlie;
+	dProd = delta;
+	eProd = echo;
+	consumer = inQueue;
+	stop = false;
+	forks = new boolean[5];
 
     }
     
     public void run(){
 
+	while(!stop){
 
+	    
+
+	}
 
     }
 
+    public void setStop(boolean flag){
+	stop = flag;
+    }
+
+    private void pickUp(int id){
+	if(id == 0){
+	    if(forks[0]&&forks[1]){
+
+	    }
+	    else{
+
+	    }
+	}
+	else if(id == 1){
+	    if(forks[2]&&forks[1]){
+
+	    }
+	    else{
+
+	    }
+	}
+	else if(id == 2){
+	    if(forks[2]&&forks[3]){
+
+	    }
+	    else{
+
+	    }
+	}
+	else if(id == 3){
+	    if(forks[3]&&forks[4]){
+
+	    }
+	    else{
+
+	    }
+	}
+	else if(id == 4){
+	    if(forks[0]&&forks[4]){
+
+	    }
+	    else{
+
+	    }
+	}
+    }
+    private void release(int id){
+	if(id == 0){
+
+	}
+	else if(id == 1){
+
+	}
+	else if(id == 2){
+
+	}
+	else if(id == 3){
+
+	}
+	else if(id == 4){
+
+	}
+    }
 }

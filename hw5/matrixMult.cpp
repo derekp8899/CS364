3676a8f4787457b88394a2ec5096440abde7db50
Derek P <>
2019-04-17 13:11:12 -0500

    added in mult for matrix and init for two from commandline

--- hw5/matrixMult.cpp
+++ hw5/matrixMult.cpp
@@ -30,23 +30,57 @@ int main(int argc, char* argv[]){
   for(int i = 0; i < size; i++)
     for(int j = 0; j < size; j++)
       M[i][j]=1;
+  
   //verify the matrix init
+  /*
   for(int i = 0; i < size; i++){
     for(int j = 0; j < size; j++)
       printf("%d ",M[i][j]);
     printf("\n");
   }
+  */
   N = (int**)malloc(size*sizeof(int*));
   for(int i = 0; i < size; i++)
     N[i]=(int*)malloc(size*sizeof(int));
   for(int i = 0; i < size; i++)
     for(int j = 0; j < size; j++)
-      N[i][j]=1;
+      N[i][j]=1
+	;
   //verify the matrix init
+  /*
   for(int i = 0; i < size; i++){
     for(int j = 0; j < size; j++)
       printf("%d ",N[i][j]);
     printf("\n");
   }
-      
+  */
+  //init the output matrix
+  int **S;
+  S = (int**)malloc(size*sizeof(int*));
+  for(int i = 0; i < size; i++)
+    S[i]=(int*)malloc(size*sizeof(int));
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < size; j++)
+      S[i][j]=0;
+
+  int rSum;
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      rSum = 0;
+      for(int k = 0;k < size; k++)
+	rSum+=(M[i][k])*(N[k][j]);
+	  
+    S[i][j]=rSum;
+    }
+  }
+  if(print==1)
+    for(int i = 0; i < size; i++){
+      for(int j = 0; j < size; j++){
+	printf("%d ",S[i][j]);
+      }
+      printf("\n");
+    }
 }
+
+  
+

7b4a1729c3e362467496b24020e779c02d4b4d6b
Derek P <>
2019-04-17 12:35:45 -0500

    added in the hw5 files

---
+++ hw5/README.txt
@@ -0,0 +1,10 @@
+Derek Popowski
+derek.a.popowski@und.edu
+CSci 364 Concurrent and distributed programming
+hw 5
+Matrix multiplication with openMP
+
+contains one source file, make file and this readme
+matrixMult.cpp
+Makefile
+README.txt
---
+++ hw5/matrixMult.cpp
@@ -0,0 +1,52 @@
+/*--------------------------------------------------
+
+Derek Popowski
+derek.a.popowski@und.edu
+CSci 364 Concurrent and distributed programming
+hw 5
+Matrix multiplication with openMP
+
+--------------------------------------------------*/
+#include <iostream>
+#include <random>
+
+int main(int argc, char* argv[]){
+
+  int print = 0;
+  
+  if(argc > 2 && (*argv[2] == 'y' || *argv[2] == 'Y' || *argv[2] == '1')){
+    print = 1;
+  }
+  int size = 0;
+  sscanf(argv[1],"%d",&size);
+  //printf("matrix size = %d\nprint : %d\n",size,print);//debug for print set and matrix size
+
+  //init the matrix to the correct size and set all values to 1
+  int **M;
+  int **N;
+  M = (int**)malloc(size*sizeof(int*));
+  for(int i = 0; i < size; i++)
+    M[i]=(int*)malloc(size*sizeof(int));
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < size; j++)
+      M[i][j]=1;
+  //verify the matrix init
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++)
+      printf("%d ",M[i][j]);
+    printf("\n");
+  }
+  N = (int**)malloc(size*sizeof(int*));
+  for(int i = 0; i < size; i++)
+    N[i]=(int*)malloc(size*sizeof(int));
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < size; j++)
+      N[i][j]=1;
+  //verify the matrix init
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++)
+      printf("%d ",N[i][j]);
+    printf("\n");
+  }
+      
+}
